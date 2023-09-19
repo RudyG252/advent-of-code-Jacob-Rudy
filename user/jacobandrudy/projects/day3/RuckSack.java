f9aa8b76598a2e5cde5256d9b57b66d97f5e5652
RudyG252 <122180943+RudyG252@users.noreply.github.com>
2023-09-19 11:41:55 -0700

    English coding, make findDuplicate and start logic to add together values of duplicates.

--- user/jacobandrudy/projects/day3/RuckSack.java
+++ user/jacobandrudy/projects/day3/RuckSack.java
@@ -1,12 +1,33 @@
 package user.jacobandrudy.projects.day3;
 import java.util.ArrayList;
+import java.util.HashSet;
 public class RuckSack {
     public static void main(String args[]){
+        int sol = 0;
         String input = Input.createInput();
         ArrayList<ArrayList<String>> compartments = seperateCompartments(input);
-        
+        for (int i = 0; i < compartments.size(); i++) {
+            int duplicate = findDuplicate(compartments.get(i));
+        }
     }
 
+    public static Character findDuplicate(ArrayList<String> input) {
+        HashSet<Character> chars = new HashSet<Character>();
+        String firstHalf = input.get(0);
+        String secondHalf = input.get(1);
+        for (int i = 0; i < half.length(); i++) {
+            chars.add(firstHalf.charAt(i));
+        }
+        for (int i = 0; i < secondHalf.length(); i++) {
+            if (chars.contains(secondHalf.charAt(i)) {
+                return secondHalf.charAt(i);
+            }
+        }
+        System.out.println("error because I coded this in English on browswer github probably");
+        return null;
+    }
+
+    
     public static ArrayList<ArrayList<String>> seperateCompartments(String input) {
         ArrayList<ArrayList<String>> sol = new ArrayList<ArrayList<String>>();
         int last = 0;
@@ -16,19 +37,17 @@ public class RuckSack {
                 // last should be the start of the compartment
                 int half = (last + ((i - last + 1)/2));
                 sol.add(new ArrayList<String>());
-                if (current <= 9) {
-                    // System.out.println(half);
-                }
-                // half + 1 because end of substring is exclusive
                 sol.get(current).add(input.substring(last, half));
                 sol.get(current).add(input.substring(half, i));
                 last = i;
                 current++;
             }
         }
-        System.out.println(sol.get(0).get(0).length());
-        System.out.println(sol.get(0).get(1).length());
-        System.out.println();
+        for (int i = 0; i < 10; i++) {
+            System.out.println(sol.get(i).get(0).length());
+            System.out.println(sol.get(i).get(1).length());
+            System.out.println();
+        }
         return sol;
     }
 }
