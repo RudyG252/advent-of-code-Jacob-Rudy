725ac3a7e3354bc47dfe8320252afba8e3203e15
RudyG252 <rudyagood@gmail.com>
2023-09-25 10:51:26 -0700

    need to finish line 29 :D

--- user/jacobandrudy/projects/day7/AddDirectories.java
+++ user/jacobandrudy/projects/day7/AddDirectories.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 public class AddDirectories {
 
     public static void main (String[] args) {
-        HashMap<String, Integer> directorySizes = new HashMap<>();
+        HashMap<String, Integer> directorySizes = new HashMap<String, Integer>();
         String currentDirectory = "";
         String input = Input.input();
-        ArrayList<String> directories = new ArrayList<>();
+        ArrayList<String> directories = new ArrayList<String>();
         int last = 0;
         boolean lookForFile = false;
         for (int i = 0; i < input.length(); i++) {
@@ -16,7 +16,7 @@ public class AddDirectories {
                     // checks for cd
                     lookForFile = false;
                     if (input.charAt(last + 2) == 'c') {
-                        if (input.substring(last + 5, last + 7) == '.') {
+                        if (input.substring(last + 5, last + 7) == "..") {
                             directories.remove(directories.size() - 1);
                         }
                         else {
@@ -26,8 +26,8 @@ public class AddDirectories {
                 }
                 else if (input.charAt(last) != 'd') {
                     String size = "";
-                    for (int j = last, input.charAt(j) != ' ', j++) {
-                        size.add(input.charAt(j));
+                    for (int j = last; input.charAt(j) != ' '; j++) {
+                        size.add(input.charAt(j) + "");
                     }
                 
 
